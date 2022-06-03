e3585014b291d87375802cf41c953f29e3230c86
Lee <dj-luder@hotmail.com>
2022-06-03 01:30:25 +0100

    auto readme.md generator via python and integration into latex compiler
    Signed-off-by: Lee <dj-luder@hotmail.com>

--- Latex_Files/Main/z_output/readme_gen.py
+++ Latex_Files/Main/z_output/readme_gen.py
@@ -4,7 +4,7 @@ import sys
 
 pdf_file = sys.argv[1]
 output_dir = sys.argv[2]
-readme_dir = sys.argv[3]
+readme_dir = "C:\\Users\\dj-lu\OneDrive - University of Exeter\\University of Exeter\\05 - Fifth Year\\Mathematics in Business Project\\"
  
  
 # Store Pdf with convert_from_path function
@@ -16,5 +16,7 @@ for i in range(len(images)):
     #Save pages as images in the pdf
     images[i].save(output_dir + 'page'+ str(i) +'.jpg', 'JPEG')
     #String for README.md
-    txt_out.append("![img](Latex_Files/Main/z_output/Images/page" + i + ".jpg)")
+    txt_out.append("![page" + str(i) + "](Latex_Files/Main/z_output/Images/page" + str(i) + ".jpg)")
 
+with open(readme_dir + "README.md", "w") as output:
+    output.write("\n".join(txt_out))
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Mathematics-in-Business-Project
 
-![This is an image](Latex_Files/Main/z_output/Images/page0.jpg)
-![This is an image](Latex_Files/Main/z_output/Images/page1.jpg)
-![This is an image](Latex_Files/Main/z_output/Images/page2.jpg)
-![This is an image](Latex_Files/Main/z_output/Images/page3.jpg)
-![This is an image](Latex_Files/Main/z_output/Images/page4.jpg)
+![page0](Latex_Files/Main/z_output/Images/page0.jpg)
+![page1](Latex_Files/Main/z_output/Images/page1.jpg)
+![page2](Latex_Files/Main/z_output/Images/page2.jpg)
+![page3](Latex_Files/Main/z_output/Images/page3.jpg)
+![page4](Latex_Files/Main/z_output/Images/page4.jpg)
